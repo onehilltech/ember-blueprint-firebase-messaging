af2d1fbb81bcbf59e3993b6f26d752380771e9d5
James Hill <james@onehilltech.com>
2021-06-25 12:47:09 -0400

    feat: migrated configuration information to a service

--- addon/adapters/application.js
+++ addon/adapters/application.js
@@ -1,30 +1,15 @@
 import RESTAdapter from '@ember-data/adapter/rest';
-import { getOwner } from '@ember/application';
 import { inject as service } from '@ember/service';
-import { isPresent } from '@ember/utils';
-import { tracked } from "@glimmer/tracking";
 
 export default class ApplicationAdapter extends RESTAdapter {
   @service
   session;
 
-  @tracked
-  _host;
+  @service
+  firebase;
 
   get host () {
-    if (isPresent (this._host)) {
-      return this._host;
-    }
-    else {
-      const ENV = getOwner (this).resolveRegistration ('config:environment');
-      const { firebase } = ENV;
-
-      return firebase.baseUrl;
-    }
-  }
-
-  set host (value) {
-    this._host = value;
+    return this.firebase.baseUrl;
   }
 
   get headers () {
---
+++ addon/services/firebase.js
@@ -0,0 +1,27 @@
+import Service from '@ember/service';
+
+import { isPresent } from '@ember/utils';
+import { getOwner } from '@ember/application';
+import { tracked } from "@glimmer/tracking";
+
+export default class FirebaseService extends Service {
+  @tracked
+  _baseUrl;
+
+  get baseUrl () {
+
+    if (isPresent (this._baseUrl)) {
+      return this._baseUrl;
+    }
+    else {
+      const ENV = getOwner (this).resolveRegistration ('config:environment');
+      const { firebase } = ENV;
+
+      return firebase.baseUrl;
+    }
+  }
+
+  set baseUrl (value) {
+    this._baseUrl = value;
+  }
+}
