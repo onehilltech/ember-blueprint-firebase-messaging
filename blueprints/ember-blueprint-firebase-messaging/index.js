fb63095d17fcd76bd80ebe218bb6bf11a0cbd505
James Hill <james@onehilltech.com>
2022-01-21 07:00:41 -0500

    fix: add-on did not install cordova plugin

--- blueprints/ember-blueprint-firebase-messaging/index.js
+++ blueprints/ember-blueprint-firebase-messaging/index.js
@@ -1,9 +1,16 @@
 /* eslint-env node */
 
 const { Blueprint } = require ('ember-cli-blueprint-helpers');
+const ecInstaller   = require ('ember-cordova-installer');
 
 module.exports = Blueprint.extend ({
   addons: [
     { name: 'ember-cli-gatekeeper' }
-  ]
+  ],
+
+  async afterInstall () {
+    await this._super.call (this, ...arguments);
+
+    ecInstaller.install ('cordova-plugin-firebasex', this);
+  }
 });
