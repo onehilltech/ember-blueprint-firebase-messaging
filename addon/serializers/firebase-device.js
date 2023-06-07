002a195df32495d78c9718a07b20a6d56284ae8b
James Hill <hilljh82@gmail.com>
2023-06-07 14:52:49 -0400

    chore: removed the custom payload key to model definitions

--- addon/serializers/firebase-device.js
+++ addon/serializers/firebase-device.js
@@ -1,11 +1,5 @@
 import ApplicationSerializer from './application';
 
 export default class FirebaseDeviceSerializer extends ApplicationSerializer {
-  payloadKeyFromModelName () {
-    return 'device';
-  }
 
-  modelNameFromPayloadKey () {
-    return 'firebase-device';
-  }
 }
