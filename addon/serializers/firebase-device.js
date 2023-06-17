0406d6cb45d3c787bd0ac536c81b60c486731c7e
James Hill <hilljh82@gmail.com>
2023-06-16 23:23:31 -0400

    fix: dasherize the payload key

--- addon/serializers/firebase-device.js
+++ addon/serializers/firebase-device.js
@@ -1,5 +1,8 @@
 import ApplicationSerializer from './application';
+import { dasherize } from '@ember/string';
 
 export default class FirebaseDeviceSerializer extends ApplicationSerializer {
-
+  payloadKeyFromModelName(modelName) {
+    return dasherize (modelName);
+  }
 }
